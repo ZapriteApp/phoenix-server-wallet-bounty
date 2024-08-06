9609189b03bed37d1c2d48fa5e6b6796d8aeeab6
hkarani <hezronkaranii@gmail.com>
2024-08-06 16:18:01 +0300

    Fetch balance from locally

--- public/script.js
+++ public/script.js
@@ -1,11 +1,18 @@
-function getBalance() {
-    fetch('/api/balance')
-      .then(response => response.json())
-      .then(data => {
-        document.getElementById('balance').innerText = 'Balance: ' + data.balance;
-      })
-      .catch(error => {
-        console.error('Error fetching balance:', error);
-      });
-  }
+window.document.getElementById('fetchBalanceBtn').addEventListener('click', () => {
+  fetch('/api/getbalance')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      document.getElementById('balance').innerText = 'Balance: ' + data.balanceSat;
+    })
+    .catch(error => {
+      console.error('Error fetching balance:', error);
+    });
+});
+
+
   
--- server.js
+++ server.js
@@ -1,15 +1,58 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+const { Buffer } = require('buffer')
+// const fetch = require('node-fetch').fetch
+const path = require('path');
+
+const corsOptions = {
+  origin: 'http://127.0.0.1:9740', 
+  methods: 'GET,POST,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization'
+};
+
+app.use(cors(corsOptions));
+
 
 // Middleware to serve static files
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  // res.send('Hello World');
+});
+
+app.get('/api/getbalance', async (req, res) => {
+  const url = 'http://127.0.0.1:9740/getbalance';
+  const username = ''; // Username if required, otherwise leave it empty
+  const password = 'd04c7770c202ec95996fd74b89bcafc7e737c687093a945d906cfbb656115e03'; // Your password
+
+  const headers = {
+    'Authorization': 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64')
+  
+  }
+
+  try {
+    const response = await fetch(url, { headers });
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Error fetching balance: ' + error.message });
+    
+  }
+});
 
 // Endpoint to interact with Phoenix Server
 app.get('/api/balance', async (req, res) => {
   try {
-    const response = await axios.get('http://phoenixd:port/balance');
+    const response = await axios.get('http://127.0.0.1:9740/getBalance');
     res.json(response.data);
   } catch (error) {
     res.status(500).send('Error fetching balance');
