4ba15f63da28944292110239b16fb4557add0059
hkarani <hezronkaranii@gmail.com>
2024-08-08 16:46:18 +0300

    Modal for Send button

--- public/script.js
+++ public/script.js
@@ -29,9 +29,55 @@
   
 // });
 
+sendButton = document.getElementById("sendButton")
+receiveButton = document.getElementById("receiveButton")
+
+// alertButton.addEventListener('click', function() {
+//   alert('You clicked receive button');
+// });
+
+receiveButton.addEventListener('click', function() {
+  alert('You clicked send button');
+});
 
 
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("sendButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Handle the radio button selection
+document.getElementById('submitOption').addEventListener('click', function() {
+  var selectedOption = document.getElementById('options').value;
+    if (selectedOption) {
+        console.log(selectedOption)
+        document.getElementById('selectedOption').textContent = 'You selected: ' + selectedOption.value;
+    } else {
+        document.getElementById('selectedOption').textContent = 'No option selected.';
+    }
+    modal.style.display = "none"; // Close the modal
+})
     
 
   
