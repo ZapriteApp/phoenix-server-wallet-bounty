81d460fcf03fa8bec704b634cedb46c19de2e8c5
hkarani <hezronkaranii@gmail.com>
2024-08-22 15:51:52 +0300

    Add offer to .env

--- utils/utils.js
+++ utils/utils.js
@@ -1,1 +1,19 @@
 require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+
+function saveToEnvFile(key, value) {
+  const envVar = `${key}=${value}\n`;
+  const envFilePath = path.resolve(__dirname, '.env');
+
+  fs.appendFile(envFilePath, envVar, (err) => {
+    if (err) {
+      console.error('Error writing to .env file:', err);
+    } else {
+      console.log(`Successfully added ${key} to .env file.`);
+    }
+  });
+}
+
+module.exports = { saveToEnvFile };
